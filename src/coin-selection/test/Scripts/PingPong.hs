ff60df4cb66fa055b410cd06cbe8990f16828dd2
Bogdan Manole <manoleb@gmail.com>
2025-12-15 13:13:20 +0200

    test(ping-pong): unit tests over the contract

--- src/coin-selection/test/Scripts/PingPong.hs
+++ src/coin-selection/test/Scripts/PingPong.hs
@@ -8,7 +8,6 @@
 -- A plutus validator that only succeeds if the redeemer is identical to the script's input index
 module Scripts.PingPong (
   validator,
-  validatorSimplified,
   PingPongRedeemer (..),
   PingPongState (..),
 ) where
@@ -21,8 +20,8 @@ import PlutusLedgerApi.V3.Contexts (
   TxInInfo (TxInInfo, txInInfoResolved),
   TxInfo (..),
  )
-import PlutusTx (Data (..), unstableMakeIsData)
-import PlutusTx.AssocMap (Map, lookup, toList)
+import PlutusTx (unstableMakeIsData)
+import PlutusTx.AssocMap (Map, lookup)
 import PlutusTx.Builtins.Internal qualified as BI
 import PlutusTx.IsData.Class (UnsafeFromData (unsafeFromBuiltinData))
 import PlutusTx.Prelude (BuiltinData, BuiltinUnit)
@@ -39,10 +38,8 @@ showState Pinged = "Pinged "
 showState Ponged = "Ponged "
 showState Stopped = "Stopped "
 
--- deriving anyclass (ToJSON, FromJSON)
-
 data PingPongRedeemer = Ping | Pong | Stop
-  deriving stock (Haskell.Show)
+  deriving stock (Haskell.Eq, Haskell.Show)
 
 {-# INLINEABLE showAction #-}
 showAction :: PingPongRedeemer -> P.BuiltinString
@@ -82,30 +79,10 @@ validator
     (Ponged, Stop, Stopped) -> BI.unitval
     _ -> P.traceError P.$ "Invalid state transition: " `P.appendString` showState currentState `P.appendString` showAction action `P.appendString` showState nextState
 
-{-# INLINEABLE validatorSimplified #-}
-validatorSimplified :: BuiltinData -> BuiltinUnit
-validatorSimplified
-  ( unsafeFromBuiltinData ->
-      ScriptContext
-        { scriptContextRedeemer = (unsafeFromBuiltinData P.. getRedeemer -> action :: PingPongRedeemer)
-        , scriptContextTxInfo =
-          txInfo@TxInfo
-            { -- txInfoInputs = (getStateFromInputs -> currentState)
-            txInfoOutputs = (getStateFromOutpusts (txInfoData txInfo) -> nextState)
-            }
-        }
-    ) = case (action, nextState) of
-    (Pong, Ponged) -> BI.unitval
-    (Ping, Pinged) -> BI.unitval
-    (Stop, Stopped) -> BI.unitval
-    _ -> P.traceError "Invalid state transition"
-
 {-# INLINEABLE getStateFromInputs #-}
 getStateFromInputs :: Map DatumHash Datum -> [TxInInfo] -> PingPongState
 getStateFromInputs _ [] = P.traceError "No inputs"
 getStateFromInputs txInfoData' (TxInInfo{txInInfoResolved = TxOut{txOutDatum}} : _) = getPingPongState txInfoData' "Datum on input" txOutDatum
-
--- getStateFromInputs _ _ = P.traceError "Multiple inputs"
 
 {-# INLINEABLE getStateFromOutpusts #-}
 getStateFromOutpusts :: Map DatumHash Datum -> [TxOut] -> PingPongState
@@ -119,17 +96,3 @@ getPingPongState _ errorMsg NoOutputDatum = P.traceError P.$ P.appendString erro
 getPingPongState datumMap errorMsg (OutputDatumHash hash) = case lookup hash datumMap of
   P.Just (unsafeFromBuiltinData P.. getDatum -> state :: PingPongState) -> state
   P.Nothing -> P.traceError P.$ P.appendString errorMsg " - OutputDatumHash not found in datum map"
-
--- getPingPongState errorMsg (OutputDatum datum) =
---   -- let d = getDatum datum
---   P.traceError P.$ errorMsg
-
-{-# INLINEABLE inspectDatum #-}
-inspectDatum :: BuiltinData -> P.BuiltinString
-inspectDatum d =
-  case BI.builtinDataToData d of
-    Constr i _ -> "Constructor " -- `P.appendString` showInt i
-    Map _ -> "Map"
-    List _ -> "List"
-    I _ -> "Integer"
-    B _ -> "ByteString"
